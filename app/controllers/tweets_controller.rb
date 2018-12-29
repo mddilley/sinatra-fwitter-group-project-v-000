bc7074f0e3ba566f068ad04c1db75074c1b44d94
Mike Dilley <mike.dilley@gmail.com>
2018-12-29 15:06:06 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
 
-  # get 'tweets/new' do
-  #   if is_loggedin?
-  #     erb :"/tweets/new"
-  #   else
-  #     redirect to "/login"
-  #   end
-  # end
+  get '/tweets' do
+    if is_loggedin?
+      @user = current_user
+      erb :"tweets/tweets"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get 'tweets/new' do
+    if is_loggedin?
+      erb :"/tweets/new"
+    else
+      redirect to "/login"
+    end
+  end
 
 end
